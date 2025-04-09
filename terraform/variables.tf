0174dcdf7b83609e8700364d8089170f130ecc57
normanfung <56857601+normanfung@users.noreply.github.com>
2025-04-09 23:18:13 +0800

    Updated final 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ecr_image_uri" {
-  description = "Your ECR image URI (e.g. 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repo:latest)"
-  type        = string
-}
-
 variable "image_tag" {
   type    = string
   default = "latest"
