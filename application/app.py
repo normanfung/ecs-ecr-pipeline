67b72a3898eec4764a94429fd4c5a45a98eb4c0e
normanfung <56857601+normanfung@users.noreply.github.com>
2025-04-09 22:41:49 +0800

    Added automated image push

--- application/app.py
+++ application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route("/") 
 def hello_world(): 
-    return "<p>Hello, Norman.v2!</p>" 
+    return "<p>Hello, Norman.v3!</p>" 
  
 if __name__ == '__main__': 
     app.run(host="0.0.0.0", port=8080) 
